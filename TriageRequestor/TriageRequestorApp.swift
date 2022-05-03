38ca5158e37615a6dc737891eaf41533135a1e15
Jasjeev Anand <jasjeev4@gmail.com>
2022-05-03 10:10:44 -0400

    first commit

---
+++ TriageRequestor/TriageRequestorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriageRequestorApp.swift
+//  TriageRequestor
+//
+//  Created by Jasjeev Anand on 03/05/22.
+//
+
+import SwiftUI
+
+@main
+struct TriageRequestorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
